48ada630c80ddc65705c128605ff75163067fe8b
Sam <samuwinkler@tsn.at>
2025-05-12 22:11:18 +0200

    blade video nr1

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,10 +5,19 @@
 <link rel="stylesheet"  href="/app.css">
 
 <body>
-  <?php foreach ($posts as $post) : ?>
-    <article>
-        <?php $post; ?> 
+  @foreach ( $posts as $post )
+    
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
+      <h1> 
+        <a href="/posts/{{ $post->slug }}">
+          {{ $post->title }}
+        </a>
+      </h1>
+
+      <div>
+        <?= $post->excerpt; ?>  
+      </div>
         </article>
-        <?php endforeach; ?>
+        @endforeach
 
     </body>
